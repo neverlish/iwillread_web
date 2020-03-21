ab138c678c845cd8b8b73a4e13a058e60614e489
Jinho Hyeon <neverlish@gmail.com>
2020-03-21 23:53:04 +0900

    setup authLink with token headers

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,6 @@
 import React from 'react';
+import { BASE_URL } from '../util/apollo';
 
-export default () => <div>hello!</div>
+export default () => {
+  return <a href={`${BASE_URL}/auth/google/login`}>button</a>
+}
--- src/util/apollo.ts
+++ src/util/apollo.ts
@@ -2,16 +2,33 @@ import { ApolloClient } from 'apollo-client';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import withApollo from 'next-with-apollo';
 import { createHttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
 import fetch from 'isomorphic-unfetch';
+import Cookies from 'js-cookie';
 
-const link = createHttpLink({
+export const BASE_URL = process.env.NODE_ENV === 'production'
+  ? 'https://iwlllread-server.neverlish.now.sh'
+  : 'http://localhost:4000';
+
+const httpLink = createHttpLink({
   fetch,
-  uri: process.env.NODE_ENV === 'production' ? 'https://iwlllread-server.neverlish.now.sh/graphql' : 'http://localhost:4000',
+  uri: `${BASE_URL}/graphql`,
 });
+
+const authLink = setContext((_, { headers }) => {
+  const token = Cookies.get('user');
+
+  return {
+    headers: {
+      ...headers,
+      authorization: `Bearer ${token || ''}`
+    },
+  };
+})
 
 export default withApollo(
   ({ initialState }) => new ApolloClient({
-    link,
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache().restore(initialState || {})
   })
 );
